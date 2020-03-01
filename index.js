091eff435445ec83ae614cbffd6898651a3d2942
Ionhedblom <ionysoshedblom@gmail.com>
2020-03-01 19:40:13 +0100

    Changes made

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
+// Require express library
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
-
+// Load startpage html with a get request
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/loginform.html');
 });
 
+
+
+
+app.post('/', (req, res) => {
+  res.send('Account created');
+});
+
+
+
+// Server start
 app.listen(3000, () => {
-  console.log('SERVER HAS STARTED ON PORT 3000');
+  console.log('LISTENING');
 });
